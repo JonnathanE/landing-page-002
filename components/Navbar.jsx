3bd2b4480454cd4cefd6901933913b69902da931
JonnathanE <jonnathan.espinoza@unl.edu.ec>
2022-04-19 18:57:00 -0500

    create navbar and intro page

---
+++ components/Navbar.jsx
@@ -0,0 +1,41 @@
+import Link from 'next/link';
+import styles from '../styles/Navbar.module.css';
+
+const Navbar = () => {
+	return (
+		<div className={styles.container}>
+			<Link href='/'>
+				<a>JEDE</a>
+			</Link>
+			<ul className={styles.list}>
+				<li className={styles.listItem}>
+					<Link href='/products/design'>
+						<a>DESIGN</a>
+					</Link>
+				</li>
+				<li className={styles.listItem}>
+					<Link href='/products/development'>
+						<a>DEVELOPMENT</a>
+					</Link>
+				</li>
+				<li className={styles.listItem}>
+					<Link href='/products/production'>
+						<a>PRODUCTION</a>
+					</Link>
+				</li>
+				<li className={styles.listItem}>
+					<Link href='/products/photography'>
+						<a>PHOTOGRAPHY</a>
+					</Link>
+				</li>
+				<li className={styles.listItem}>
+					<Link href='/products/contact'>
+						<a>CONTACT</a>
+					</Link>
+				</li>
+			</ul>
+		</div>
+	);
+};
+
+export default Navbar;
