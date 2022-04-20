55a103cc980c5f73b727481dc7febec1d1ac8b76
JonnathanE <jonnathan.espinoza@unl.edu.ec>
2022-04-19 20:30:25 -0500

    create services component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,10 @@
 import Head from 'next/head';
 import Intro from '../components/Intro';
 import Services from '../components/Services';
+import { data } from '../data';
 import styles from '../styles/Home.module.css';
 
-export default function Home() {
+export default function Home({ services }) {
 	return (
 		<div className={styles.container}>
 			<Head>
@@ -12,7 +13,14 @@ export default function Home() {
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
 			<Intro />
-			<Services />
+			<Services services={services} />
 		</div>
 	);
 }
+
+export const getStaticProps = async () => {
+	const services = data;
+	return {
+		props: { services },
+	};
+};
